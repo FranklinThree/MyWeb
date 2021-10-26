aa037fd784c0ba3ef4c1c758f7db776498085c6a
FranklinThree <franklin3@hdu.edu.cn>
2021-10-26 17:17:35 +0800

    2021年10月26日17:16:26

--- Resource/Server.go
+++ Resource/Server.go
@@ -1,19 +1,20 @@
 package Resource
 
 import (
+	_ "gorm.io/gorm"
+
 	_ "database/sql"
 	_ "fmt"
 	_ "github.com/gin-gonic/gin"
-	_ "github.com/go-gorm/gorm"
 	_ "github.com/go-sql-driver/mysql"
 	_ "net/http"
 	_ "strconv"
 )
 
 type AwesomeServer struct {
-
 }
-func (as *AwesomeServer)Start()(e error){
-	return e;
+
+func (as *AwesomeServer) Start() (e error) {
+	return e
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/FranklinThree/MyWeb
+module main
 
 go 1.17
