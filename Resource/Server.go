fdb8706996fb649e21fe037e8b5a1bec7debfe53
FranklinThree <franklin3@hdu.edu.cn>
2021-10-26 17:19:48 +0800

    2021年10月26日17:19:36

--- Resource/Server.go
+++ Resource/Server.go
@@ -1,12 +1,12 @@
 package Resource
 
 import (
-	_ "gorm.io/gorm"
-
 	_ "database/sql"
 	_ "fmt"
 	_ "github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	_ "gorm.io/driver/sqlite"
+	_ "gorm.io/gorm"
 	_ "net/http"
 	_ "strconv"
 )
