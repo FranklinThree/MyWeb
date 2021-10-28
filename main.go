79afff33fd153b15819bc51993a2c133f537fa09
FranklinThree <franklin3@hdu.edu.cn>
2021-10-28 08:26:22 +0800

    2021年10月28日08:12:28

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/FranklinThree/MyWeb/resource"
+	"github.com/FranklinThree/MyWeb"
+	"resource"
 )
 
 func main() {
 	fmt.Println("Good afternoon!")
-
+	//AServer :=resource.AwesomeServer{}
+	//AServer
 }
